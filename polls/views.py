a60d57f33efbf8301697a98949c75e638ce523a8
Bruno Maia <bruno_engineer@hotmail.com>
2019-11-17 11:41:26 -0300

    add - poll views, Question model admin registry

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World. Polls=Index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Checking for question: {question_id}.")
+
+
+def results(request, question_id):
+    response = "Looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"+1 vote on question {question_id}.")

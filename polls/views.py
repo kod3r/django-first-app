92a61c1338ecfc8037da6483b7d2eb57e75237c7
Bruno Maia <bruno_engineer@hotmail.com>
2019-11-17 11:56:05 -0300

    updt - index view for polls app return method

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import django.db
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. Polls=Index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):

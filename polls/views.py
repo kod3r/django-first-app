a1d72e6aa9d874c7260c1953845b15bb4ade8251
Bruno Maia <bruno_engineer@hotmail.com>
2019-11-17 16:16:11 -0300

    updt - detail view adding voting form

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
 
-# from django.template import loader
-
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -36,4 +35,23 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"+1 vote on question {question_id}.")
+    # return HttpResponse(f"+1 vote on question {question_id}.")
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "Choice not selected!",},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # TODO: IMPORTANT ==>
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

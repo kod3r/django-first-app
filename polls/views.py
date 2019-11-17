ca91824c8988d32eb0e82a62b79f9d0037ec939b
Bruno Maia <bruno_engineer@hotmail.com>
2019-11-17 16:26:35 -0300

    add - Results view and results method

--- polls/views.py
+++ polls/views.py
@@ -55,3 +55,8 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing with POST data.
         # This prevents data from being posted twice if a user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
